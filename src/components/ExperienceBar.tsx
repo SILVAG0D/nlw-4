6b1c777ffa2323faa251dc1b1835dd30e87d2d1e
SILVAG0D <70017736+SILVAG0D@users.noreply.github.com>
2021-02-26 23:09:53 -0300

    aplicação finalizada e com o darkmode implementado

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,6 +1,8 @@
-import { useContext } from 'react';
+import { useTheme } from 'next-themes';
+import { useContext, useEffect, useState } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ExperienceBar.module.css';
+import Switch from "react-switch";
 
 
 export function  ExperienceBar(){
@@ -8,19 +10,47 @@ export function  ExperienceBar(){
 
     const percentToNextLevel = Math.round((currentExperience*100)) / experienceToNextLevel;
 
-    
+    const {theme, setTheme}  = useTheme();
+
+    const [mounted,setMounted] = useState(false);
+
+    function toogleTheme() {
+      if(theme === 'dark') {
+        setTheme('light');
+      }else {
+        setTheme('dark');
+      }
+    }
+
+    useEffect(() => setMounted(true),[])
+
+    if (!mounted) return null;
 
     return (
-        <header className={styles.experienceBar}> 
-            <span>0 xp</span>
-            <div>
-                <div style={{width: `${percentToNextLevel}%`}}/>
+        <div className={styles.menu}>
 
-                <span className={styles.currentExperience} style={{left: `${percentToNextLevel}%`}}>{currentExperience} xp</span>
-               
-            </div>
-            <span>{experienceToNextLevel} xp</span>
-
+        <header className={styles.experienceBar}>
+          <span>0 xp</span>
+          <div>
+            <div style={{ width: `${percentToNextLevel}%` }} />
+  
+            <span className={styles.currentExperience} style={{ left: `${percentToNextLevel}%` }}>
+              {currentExperience}
+              {' '}
+              xp
+            </span>
+          </div>
+          <span>
+            {experienceToNextLevel}
+            {' '}
+            xp
+          </span>
         </header>
+  
+        <div>
+        <Switch className={styles.switch} onChange={toogleTheme} checked={theme ==='dark'}  onColor="#fff" offColor="#000" onHandleColor="#000"/>
+        </div>
+  
+      </div>
     );
 } 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import '../styles/global.css';
-
+import { ThemeProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }) {
 
   return(        
+      <ThemeProvider>
        <Component {...pageProps} />
-             
+       </ThemeProvider>
   )
 }
 
